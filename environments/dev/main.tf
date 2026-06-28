f5b6e082e9ca7df4ba08ac14f33df388c09535e8
Naman <rohitasnaman09@gmail.com>
2026-06-29 01:11:02 +0530

    add resuable lambda and wire ingest function to it

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,3 +13,28 @@ output "bucket_arn" {
   description = "ARN of the data bucket"
   value       = module.data_bucket.bucket_arn
 }
+# Build an IAM policy that lets the function write to OUR bucket
+data "aws_iam_policy_document" "ingest_permissions" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["${module.data_bucket.bucket_arn}/*"]
+  }
+}
+
+module "ingest_fn" {
+  source = "../../modules/lambda"
+
+  function_name = "${var.project}-${var.environment}-ingest"
+  source_dir    = "${path.module}/../../src/ingest"
+  handler       = "index.handler"
+
+  environment_vars = {
+    BUCKET_NAME = module.data_bucket.bucket_id
+  }
+
+  additional_policy_json = data.aws_iam_policy_document.ingest_permissions.json
+}
+
+output "ingest_function_name" {
+  value = module.ingest_fn.function_name
+}
